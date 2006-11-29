80acd7ace68fa6b4b8b1a6b0cc76bcdd1b891c4d
poftwaresatent <poftwaresatent@86aca762-15d7-0310-9726-9bd8562223d8>
2006-11-29 15:23:04 +0000

    added traversability map parsing and an example map, will now move this code to common/World and add a traversability drawing class
    git-svn-id: https://libsunflower.svn.sourceforge.net/svnroot/libsunflower/trunk/nepumuk@370 86aca762-15d7-0310-9726-9bd8562223d8

--- simul/nepumuk.cpp
+++ simul/nepumuk.cpp
@@ -28,7 +28,12 @@
 #include <npm/common/World.hpp>
 #include <npm/common/wrap_glut.hpp>
 #include <sfl/util/Pthread.hpp>
+#include <sfl/util/array2d.hpp>
+#include <sfl/util/Line.hpp>
+#include <sfl/gplan/GridFrame.hpp>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <signal.h>
 
 
@@ -44,7 +49,8 @@ public:
   Parameters():
     robot_config_filename("robots.config"),
     layout_config_filename("layout.config"),
-    world_name("stage"),
+    world_name(""),
+    traversability_filename(""),
     //    threaded(false),
     no_glut(false),
     fatal_warnings(false)
@@ -54,9 +60,18 @@ public:
   string robot_config_filename;
   string layout_config_filename;
   string world_name;
+  string traversability_filename;
   //  bool threaded;
   bool no_glut;
   bool fatal_warnings;
+};
+
+
+struct travmap {
+  double resolution, origin_x, origin_y, origin_theta;
+  int freespace, obstacle;//, default;
+  string worldname;
+  shared_ptr<array2d<int> >  data;
 };
 
 
@@ -75,6 +90,10 @@ static void keyboard(unsigned char key, int x, int y);
 static void timer(int handle);
 static void cleanup();
 static void sighandle(int signum);
+
+static shared_ptr<travmap> parse_traversability(istream & is, ostream & os);
+static void apply_traversability(shared_ptr<const travmap> travmap,
+				 shared_ptr<World> world);
 
 
 int main(int argc, char ** argv)
@@ -95,9 +114,37 @@ int main(int argc, char ** argv)
   
   parse_options(argc, argv);
   
-  shared_ptr<World> world(World::Create(params.world_name));
-  if( ! world){
-    cerr << "Invalid World \"" << params.world_name << "\"\n";
+  shared_ptr<World> world;
+  shared_ptr<travmap> traversability;
+  if( ! params.world_name.empty()){
+    if( ! params.traversability_filename.empty()){
+      cerr << "ERROR: specify either world name or traversability file.\n";
+      exit(EXIT_FAILURE);
+    }
+    world = World::Create(params.world_name);
+    if( ! world){
+      cerr << "Invalid World \"" << params.world_name << "\"\n";
+      exit(EXIT_FAILURE);
+    }
+  }
+  else if( ! params.traversability_filename.empty()){
+    ifstream trav(params.traversability_filename.c_str());
+    if( ! trav){
+      cerr << "ERROR: invalid traversability file \""
+	   << params.traversability_filename << "\".\n";
+      exit(EXIT_FAILURE);
+    }
+    traversability = parse_traversability(trav, cerr);
+    if( ! traversability){
+      cerr << "ERROR: parsing traversability file \""
+	   << params.traversability_filename << "\".\n";
+      exit(EXIT_FAILURE);
+    }
+    world.reset(new World(traversability->worldname));
+    apply_traversability(traversability, world);
+  }
+  else{
+    cerr << "ERROR: Specify either a world or a traversability file.\n";
     exit(EXIT_FAILURE);
   }
   
@@ -194,6 +241,8 @@ void parse_options(int argc,
 			 'r', "robot", "Name of the robot config file."));
   ilock.Add(new StringCB(params.world_name,
 			 'w', "world", "Name of the world (expo|mini|tta)."));
+  ilock.Add(new StringCB(params.traversability_filename,
+			 'm', "travmap", "Traversability map file."));
 //   ilock.
 //     Add(new Interlock::BoolCallback(params.threaded, 't', "threaded",
 // 				    "Use separate GLUT thread."));
@@ -228,4 +277,197 @@ void sighandle(int signum)
 {
   cerr << "signal " << signum << ": exit\n";
   exit(EXIT_SUCCESS);
+}
+
+
+shared_ptr<travmap>
+parse_traversability(istream & is, ostream & os)
+{
+  shared_ptr<travmap> result(new travmap());
+  result->resolution = 0.1;
+  result->origin_x = 0.0;
+  result->origin_y = 0.0;
+  result->origin_theta = 0.0;
+  result->obstacle = 255;
+  result->freespace = 0;
+  result->worldname = "world";
+  int thedefaultvalueman(0);
+  
+  vector<vector<int> > data;
+  size_t grid_xsize(0);
+  int maxdata(numeric_limits<int>::min());
+  int mindata(numeric_limits<int>::max());
+  
+  string textline;
+  while(getline(is, textline)){
+    istringstream tls(textline);
+    if(textline[0] == '#'){
+      tls.ignore(1, '\n');
+      string token;
+      if(tls >> token){
+	if(token == "resolution"){
+	  tls >> result->resolution;
+	  if( ! tls){
+	    cerr << "ERROR: could not parse resolution from \""
+		 << tls.str() << "\"\n";
+	    return shared_ptr<travmap>();
+	  }
+	}
+	else if(token == "origin"){
+	  tls >> result->origin_x >> result->origin_y >> result->origin_theta;
+	  if( ! tls){
+	    cerr << "ERROR: could not parse origin from \""
+		 << tls.str() << "\"\n";
+	    return shared_ptr<travmap>();
+	  }
+	}
+	else if(token == "obstacle"){
+	  tls >> result->obstacle;
+	  if( ! tls){
+	    cerr << "ERROR: could not parse obstacle from \""
+		 << tls.str() << "\"\n";
+	    return shared_ptr<travmap>();
+	  }
+	}
+	else if(token == "freespace"){
+	  tls >> result->freespace;
+	  if( ! tls){
+	    cerr << "ERROR: could not parse freespace from \""
+		 << tls.str() << "\"\n";
+	    return shared_ptr<travmap>();
+	  }
+	}
+	else if(token == "default"){
+	  //	  tls >> result->default;
+	  tls >> thedefaultvalueman;
+	  if( ! tls){
+	    cerr << "ERROR: could not parse default from \""
+		 << tls.str() << "\"\n";
+	    return shared_ptr<travmap>();
+	  }
+	}
+	else if(token == "name"){
+	  tls >> result->worldname;
+	  if( ! tls){
+	    cerr << "ERROR: could not parse name from \""
+		 << tls.str() << "\"\n";
+	    return shared_ptr<travmap>();
+	  }
+	  if(result->worldname.empty()){
+	    cerr << "ERROR: empty names not allowed (from \""
+		 << tls.str() << "\")\n";
+	    return shared_ptr<travmap>();
+	  }
+	}
+      }
+      continue;
+    }
+    
+    data.push_back(vector<int>());
+    vector<int> & line(*data.rbegin());
+    int value;
+    while(tls >> value){
+      line.push_back(value);
+      if(value > maxdata)
+	maxdata = value;
+      if(value < mindata)
+	mindata = value;
+    }
+    if(line.size() > grid_xsize)
+      grid_xsize = line.size();
+  }
+  const size_t grid_ysize(data.size());
+  
+  if(grid_xsize < 1){
+    os << "ERROR in parse_traversability(): grid_xsize == " << grid_xsize
+       << "\n";
+    return shared_ptr<travmap>();
+  }
+  if(grid_ysize < 1){
+    os << "ERROR in parse_traversability(): grid_ysize == " << grid_ysize
+       << "\n";
+    return shared_ptr<travmap>();
+  }
+  
+  //result->data.reset(new array2d<int>(grid_xsize, grid_ysize, result->default));
+  result->data.reset(new array2d<int>(grid_xsize, grid_ysize, thedefaultvalueman));
+  for(size_t iy(0); iy < grid_ysize; ++iy){
+    vector<int> & line(data[iy]);
+    for(size_t ix(0); ix < line.size(); ++ix)
+      (*result->data)[ix][iy] = line[ix];
+  }
+  
+  return result;
+}
+
+
+void add_line_n(size_t ix, size_t iy, const GridFrame & gframe,
+		shared_ptr<World> world)
+{
+  const double offset(gframe.GetDelta() / 2);
+  GridFrame::position_t center(gframe.LocalPoint(ix, iy));
+  Line line(center.v0 - offset, center.v1 + offset,
+	    center.v0 + offset, center.v1 + offset);
+  line.TransformTo(gframe);
+  world->AddLine(line);
+}
+
+
+void add_line_e(size_t ix, size_t iy, const GridFrame & gframe,
+		shared_ptr<World> world)
+{
+  const double offset(gframe.GetDelta() / 2);
+  GridFrame::position_t center(gframe.LocalPoint(ix, iy));
+  Line line(center.v0 + offset, center.v1 - offset,
+	    center.v0 + offset, center.v1 + offset);
+  line.TransformTo(gframe);
+  world->AddLine(line);
+}
+
+
+void add_line_s(size_t ix, size_t iy, const GridFrame & gframe,
+		shared_ptr<World> world)
+{
+  const double offset(gframe.GetDelta() / 2);
+  GridFrame::position_t center(gframe.LocalPoint(ix, iy));
+  Line line(center.v0 - offset, center.v1 - offset,
+	    center.v0 + offset, center.v1 - offset);
+  line.TransformTo(gframe);
+  world->AddLine(line);
+}
+
+
+void add_line_w(size_t ix, size_t iy, const GridFrame & gframe,
+		shared_ptr<World> world)
+{
+  const double offset(gframe.GetDelta() / 2);
+  GridFrame::position_t center(gframe.LocalPoint(ix, iy));
+  Line line(center.v0 - offset, center.v1 - offset,
+	    center.v0 - offset, center.v1 + offset);
+  line.TransformTo(gframe);
+  world->AddLine(line);
+}
+
+
+void apply_traversability(shared_ptr<const travmap> travmap,
+			  shared_ptr<World> world)
+{
+  const GridFrame gframe(travmap->origin_x, travmap->origin_y,
+			 travmap->origin_theta, travmap->resolution);
+  for(size_t ix(0); ix < travmap->data->xsize; ++ix)
+    for(size_t iy(0); iy < travmap->data->ysize; ++iy)
+      if((*travmap->data)[ix][iy] >= travmap->obstacle){
+	if((0 >= ix)
+	   || ((*travmap->data)[ix - 1][iy] < travmap->obstacle))
+	  add_line_w(ix, iy, gframe, world);
+	if((travmap->data->xsize - 1 <= ix)
+	   || ((*travmap->data)[ix + 1][iy] < travmap->obstacle))
+	  add_line_e(ix, iy, gframe, world);
+	if((0 >= iy)
+	   || ((*travmap->data)[ix][iy - 1] < travmap->obstacle))
+	  add_line_s(ix, iy, gframe, world);
+	if((travmap->data->ysize - 1 <= iy)
+	   || ((*travmap->data)[ix][iy + 1] < travmap->obstacle))
+	  add_line_n(ix, iy, gframe, world);
+      }
 }
