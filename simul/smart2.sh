3677e2287f4c41b0e4611440810568a45d211455
poftwaresatent <poftwaresatent@86aca762-15d7-0310-9726-9bd8562223d8>
2007-03-20 17:45:40 +0000

    smart3 test files: use a-priori line-based world coupled with an initially empty travmap
    git-svn-id: https://libsunflower.svn.sourceforge.net/svnroot/libsunflower/trunk/nepumuk@512 86aca762-15d7-0310-9726-9bd8562223d8

--- simul/smart2.sh
+++ simul/smart2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ARGS="-f -m travmap.smart2 -l layout.smart2 -r robots.smart2 -W world.smart2"
+ARGS="-f -t -m travmap.smart2 -l layout.smart2 -r robots.smart2"
 
 echo "ARGS: $ARGS"
 ./nepumuk $ARGS
