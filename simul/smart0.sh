3d5cdc6f98654993aeea49bda2d43b39eaa9eeb6
poftwaresatent <poftwaresatent@86aca762-15d7-0310-9726-9bd8562223d8>
2007-04-18 11:54:55 +0000

    about to replace smart0 with icra07-artificial and smart1 with icra07-logreplay
    git-svn-id: https://libsunflower.svn.sourceforge.net/svnroot/libsunflower/trunk/nepumuk@575 86aca762-15d7-0310-9726-9bd8562223d8

--- simul/smart0.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ARGS="-f -M travmap.smart0 -l layout.smart0 -r robots.smart0"
-
-echo "ARGS: $ARGS"
-./nepumuk $ARGS
--- simul/smart1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ARGS="-f -M travmap.smart1 -l layout.smart1 -r robots.smart1"
-
-echo "ARGS: $ARGS"
-./nepumuk $ARGS
