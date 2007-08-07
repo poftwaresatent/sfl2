7f15d3ea9d1ac7a75dc3f59a1a0d38c66efc0504
poftwaresatent <poftwaresatent@86aca762-15d7-0310-9726-9bd8562223d8>
2007-08-07 03:29:08 +0000

    pretty flexible build script
    git-svn-id: https://libsunflower.svn.sourceforge.net/svnroot/libsunflower/trunk/nepumuk@657 86aca762-15d7-0310-9726-9bd8562223d8

---
+++ build-stage.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+PREFIX="$PWD/stage"
+EXTRA_CFGOPTS=""
+RUN_BOOTSTRAP="yes"
+WORKDIR="build"
+
+while [ ! -z "$1" ]; do
+    case $1 in
+	-h|--help)
+echo 'build-stage.sh command line options:'
+echo '  [-p|--prefix]  <PREFIX>     install prefix ($PREFIX)'
+echo '  [-w|--work]    <DIR>        build work directory ($WORKDIR)'
+echo '  [-b|--boost]   <DIR>        BOOST library install directory'
+echo '  [-n|--png]     <DIR>        PNG install library directory'
+echo '  [-f|--sfl]     <DIR>        libsunflower install directory'
+echo '  [-d|--debug]                enable debug messages and symbols'
+echo '  [-s|--skipbs]               do not bootstrap build system'
+echo '  [-q|--qt]                   enable Qt support (experimental)'
+        exit 0;;
+	-p|--prefix)
+	    PREFIX=$2;
+	    shift; shift; continue;;
+	-w|--work)
+	    WORKDIR=$2;
+	    shift; shift; continue;;
+	-b|--boost)
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-boost=$2"
+	    shift; shift; continue;;
+	-n|--png)
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-png=$2"
+	    shift; shift; continue;;
+	-f|--sfl)
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-sfl=$2"
+	    shift; shift; continue;;
+	-d|--debug)
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --enable-debug"
+	    shift; continue;;
+	-s|--skipbs)
+	    RUN_BOOTSTRAP="no"
+	    shift; continue;;
+	-q|--qt)
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --enable-qt"
+	    shift; continue;;
+	*)
+	    echo "ERROR unhandled option(s) $*" 1>&2
+	    exit 1;;
+    esac
+done
+
+CFGOPTS="--prefix=$PREFIX $EXTRA_CFGOPTS"
+
+echo "configure options: $CFGOPTS"
+
+if [ $RUN_BOOTSTRAP = "yes" ]; then
+    echo "running bootstrap-buildsystem.sh (takes a while)"
+    ./bootstrap-buildsystem.sh
+    if [ $? -ne 0 ]; then
+	echo "ERROR bootstrap-buildsystem.sh"
+	exit 1
+    fi
+fi
+
+rm -rf $WORKDIR
+if [ ! -d $WORKDIR ]; then
+    mkdir $WORKDIR
+    if [ $? -ne 0 ]; then
+	echo "ERROR mkdir $WORKDIR"
+	exit 1
+    fi
+fi
+
+cd $WORKDIR
+../configure $CFGOPTS
+if [ $? -ne 0 ]; then
+    echo "ERROR ../configure $CFGOPTS"
+    exit 1
+fi
+
+make
+if [ $? -ne 0 ]; then
+    echo "ERROR make"
+    exit 1
+fi
+
+make install
+if [ $? -ne 0 ]; then
+    echo "ERROR make install"
+    exit 1
+fi
