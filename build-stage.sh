37359da4994c713bde76366a28e8e622fec10f2f
poftwaresatent <poftwaresatent@86aca762-15d7-0310-9726-9bd8562223d8>
2008-01-13 07:54:17 +0000

    buildsys support for OpenBSD, added -m and -j to build-stage.sh
    git-svn-id: https://libsunflower.svn.sourceforge.net/svnroot/libsunflower/trunk/nepumuk@782 86aca762-15d7-0310-9726-9bd8562223d8

--- build-stage.sh
+++ build-stage.sh
@@ -4,46 +4,71 @@ PREFIX="$PWD/stage"
 EXTRA_CFGOPTS=""
 RUN_BOOTSTRAP="yes"
 WORKDIR="build"
+EXTRA_MAKEOPTS=""
+MAKE="make"
+
+function abspath {
+    if [ foo`echo $1 | sed 's:\(.\).*:\1:'` = "foo/" ]; then
+        RETVAL=$1
+    else
+        RETVAL=`pwd`/$1
+    fi
+}
 
 while [ ! -z "$1" ]; do
     case $1 in
 	-h|--help)
-echo 'build-stage.sh command line options:'
-echo '  [-p|--prefix]  <PREFIX>     install prefix ($PREFIX)'
-echo '  [-w|--work]    <DIR>        build work directory ($WORKDIR)'
-echo '  [-b|--boost]   <DIR>        BOOST library install directory'
-echo '  [-n|--png]     <DIR>        PNG install library directory'
-echo '  [-f|--sfl]     <DIR>        libsunflower install directory'
-echo '  [-e|--estar]   <DIR>        E* install directory'
-echo '  [-a|--asl]     <DIR>        ASL install directory'
-echo '  [-d|--debug]                enable debug messages and symbols'
-echo '  [-s|--skipbs]               do not bootstrap build system'
-echo '  [-q|--qt]                   enable Qt support (experimental)'
+echo "build-stage.sh command line options:"
+echo "  [-p|--prefix]  <PREFIX>     install prefix ($PREFIX)"
+echo "  [-w|--work]    <DIR>        build work directory ($WORKDIR)"
+echo "  [-b|--boost]   <DIR>        BOOST library install directory"
+echo "  [-n|--png]     <DIR>        PNG install library directory"
+echo "  [-f|--sfl]     <DIR>        libsunflower install directory"
+echo "  [-e|--estar]   <DIR>        E* install directory"
+echo "  [-a|--asl]     <DIR>        ASL install directory"
+echo "  [-m|--make]    <PATH>       GNU Make executable (name or path)"
+echo "  [-j|--jobs]    <NUM>        number of parallel make jobs"
+echo "  [-d|--debug]                enable debug messages and symbols"
+echo "  [-s|--skipbs]               do not bootstrap build system"
+echo "  [-q|--qt]                   enable Qt support (experimental)"
         exit 0;;
 	-p|--prefix)
-	    PREFIX=$2;
+	    abspath $2
+	    PREFIX=$RETVAL;
 	    shift; shift; continue;;
 	-w|--work)
-	    WORKDIR=$2;
+	    abspath $2
+	    WORKDIR=$RETVAL;
 	    shift; shift; continue;;
 	-b|--boost)
-	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-boost=$2"
+	    abspath $2
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-boost=$RETVAL"
 	    shift; shift; continue;;
 	-n|--png)
-	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-png=$2"
+	    abspath $2
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-png=$RETVAL"
 	    shift; shift; continue;;
 	-f|--sfl)
-	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-sfl=$2"
+	    abspath $2
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-sfl=$RETVAL"
 	    shift; shift; continue;;
 	-e|--estar)
-	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-estar=$2"
+	    abspath $2
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-estar=$RETVAL"
 	    shift; shift; continue;;
 	-a|--asl)
-	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-asl=$2"
+	    abspath $2
+	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --with-asl=$RETVAL"
 	    shift; shift; continue;;
 	-d|--debug)
 	    EXTRA_CFGOPTS="$EXTRA_CFGOPTS --enable-debug"
 	    shift; continue;;
+	-m|--make)
+	    MAKE="$2"
+	    shift; shift; continue;;
+	-j|--jobs)
+	    EXTRA_MAKEOPTS="$EXTRA_MAKEOPTS -j $2"
+	    shift; shift; continue;;
 	-s|--skipbs)
 	    RUN_BOOTSTRAP="no"
 	    shift; continue;;
@@ -85,14 +110,14 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-make
+$MAKE $EXTRA_MAKEOPTS
 if [ $? -ne 0 ]; then
-    echo "ERROR make"
+    echo "ERROR $MAKE $EXTRA_MAKEOPTS"
     exit 1
 fi
 
-make install
+$MAKE $EXTRA_MAKEOPTS install
 if [ $? -ne 0 ]; then
-    echo "ERROR make install"
+    echo "ERROR $MAKE $EXTRA_MAKEOPTS install"
     exit 1
 fi
